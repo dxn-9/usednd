18e1d25ce36a8472968c2673a04102bca1c7e49b
Daniel <daniel.lauri22@gmail.com>
2023-05-06 23:39:09 +0200

    make default example 0

--- examples/simple_vite/src/App.tsx
+++ examples/simple_vite/src/App.tsx
@@ -8,12 +8,11 @@ import Scenario4 from './scenarios/4'
 const availableScenarios = [<Scenario1 />, <Scenario2 />, <Scenario3 />, <Scenario4 />]
 
 function App() {
-    const [currentScenario, setCurrentScenario] = useState(3)
+    const [currentScenario, setCurrentScenario] = useState(0)
 
     return (
         <main className="h-screen w-screen overflow-hidden flex justify-center items-center">
-
-<div style={{ position: 'absolute', top: '0', left: '0', zIndex: 9999 }}>
+            <div style={{ position: 'absolute', top: '0', left: '0', zIndex: 9999 }}>
                 {availableScenarios.map((_, i) => (
                     <button onClick={setCurrentScenario.bind(null, i)} key={i}>
                         {i}
